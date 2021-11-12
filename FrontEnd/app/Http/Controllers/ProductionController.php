7b39861c4b3f9fb35c38c6bfa3b5ea73de83a676
Nicolaj2903 <Iversen2903@hotmail.com>
2021-11-12 11:13:54 +0100

    Models, controller and .blade files are made according to the UML (currently empty)

---
+++ FrontEnd/app/Http/Controllers/ProductionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Production;
+use Illuminate\Http\Request;
+
+class ProductionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Production  $production
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Production $production)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Production  $production
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Production $production)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Production  $production
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Production $production)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Production  $production
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Production $production)
+    {
+        //
+    }
+}
