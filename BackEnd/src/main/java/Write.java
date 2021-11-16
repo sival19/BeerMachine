49cc2b8ad79428cdd853aa0d03a32aded3ac85d8
sival19 <sival19@student.sdu.dk>
2021-11-16 21:47:30 +0100

    write class in java starts production

--- BackEnd/src/main/java/Write.java
+++ BackEnd/src/main/java/Write.java
@@ -10,6 +10,7 @@
  */
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfigBuilder;
@@ -32,10 +33,37 @@ public class Write {
             OpcUaClient client = OpcUaClient.create(cfg.build());
             client.connect().get();
 
+            //reset
+            NodeId nodeId1 = NodeId.parse("ns=6;s=::Program:Cube.Command.CntrlCmd");
+            client.writeValue(nodeId1, DataValue.valueOnly(new Variant(1))).get();
+            NodeId nodeId2 = NodeId.parse("ns=6;s=::Program:Cube.Command.CmdChangeRequest");
+            client.writeValue(nodeId2, DataValue.valueOnly(new Variant(true))).get();
+
+            //set batchid to 1
+            NodeId nodeId3 = NodeId.parse("ns=6;s=::Program:Cube.Command.Parameter[0].Value");
+            client.writeValue(nodeId3, DataValue.valueOnly(new Variant((float)1.0))).get();
+
+            //set productId to 1
+            NodeId nodeId4 = NodeId.parse("ns=6;s=::Program:Cube.Command.Parameter[1].Value");
+            client.writeValue(nodeId4, DataValue.valueOnly(new Variant((float)1.0))).get();
+
+            //set amount to 10k
+            NodeId nodeId5 = NodeId.parse("ns=6;s=::Program:Cube.Command.Parameter[2].Value");
+            client.writeValue(nodeId5, DataValue.valueOnly(new Variant((float)10000.0))).get();
+
+            //set mach speed to 200
+            NodeId nodeId6 = NodeId.parse("ns=6;s=::Program:Cube.Command.MachSpeed");
+            client.writeValue(nodeId6, DataValue.valueOnly(new Variant((float)200.0))).get();
+
+            //start the shit up
+            TimeUnit.SECONDS.sleep(3);
+            NodeId nodeId7 = NodeId.parse("ns=6;s=::Program:Cube.Command.CntrlCmd");
+            client.writeValue(nodeId7, DataValue.valueOnly(new Variant(2))).get();
+            NodeId nodeId8 = NodeId.parse("ns=6;s=::Program:Cube.Command.CmdChangeRequest");
+            client.writeValue(nodeId8, DataValue.valueOnly(new Variant(true))).get();
 
 
-            NodeId nodeId = NodeId.parse("ns=6;s=::Program:Cube.Command.Parameter[0].Value");
-            client.writeValue(nodeId, DataValue.valueOnly(new Variant((float)2.0))).get();
+
 
         }
         catch(Throwable ex)
