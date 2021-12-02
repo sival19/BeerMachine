520bdd9722fd4b490d1529462242e72822c9116f
Simon Fugl <sifug20@student.sdu.dk>
2021-12-02 11:48:58 +0100

    The good shit

--- FrontEnd/resources/views/startProductionView.blade.php
+++ FrontEnd/resources/views/startProductionView.blade.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <title>
-Current Production
+        Current Production
     </title>
     <link rel="stylesheet" type="text/css" href="{{'/css/start.css'}}">
 </head>
@@ -14,24 +14,44 @@ Current Production
 <h1>Start a Production</h1>
 
 <div class="createProductionForm">
-<form method="post" action="/beginProduction">
-@csrf
-        <label for="productType" class="form-label">Select type of beer</label>
-        <select type="text" name="type" class="form-control name" id="productType" required>
+    <form method="post" action="/beginProduction">
+        @csrf
+        <p>
+        <span>
+            <label for="productType" class="form-label">Select type of beer</label>
+        </span>
+            <span>
+                <select type="text" name="type" class="form-control name" id="productType" required>
             <option value="0.0">Pilsner</option>
             <option value="1.0">Wheat</option>
             <option value="2.0">IPA</option>
             <option value="3.0">Stout</option>
             <option value="4.0">Ale</option>
             <option value="5.0">Alcohol Free</option>
-        </select>
-
-        <label for="productionAmount" class="form-label">Enter desired amount of beer to produce</label>
-        <input type="number" name="amount" class="form-control name" id="productionAmount" placeholder="1000" required>
-        <label for="machineSpeed" name="machineSpeed" class="form-label">Enter desired machine speed</label>
+            </select>
+            </span>
+        </p>
+        <p>
+        <span>
+            <label for="productionAmount" class="form-label">Enter desired amount of beer to produce</label>
+        </span>
+            <span>
+                <input type="number" name="amount" class="form-control name" id="productionAmount" placeholder="1000"
+                       required>
+            </span>
+        </p>
+        <p>
+        <span>
+            <label for="machineSpeed" name="machineSpeed" class="form-label">Enter desired machine speed</label>
+        </span>
+            <span>
         <input type="number" name="machineSpeed" class="form-control name" id="machineSpeed" placeholder="50" required>
-    <button type="submit">Create Production</button>
-</form>
+    </span>
+        </p>
+
+
+        <button type="submit">Create Production</button>
+    </form>
 
 </div>
 
