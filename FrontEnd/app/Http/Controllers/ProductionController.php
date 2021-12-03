b83354b88ffd6676d69b0f732c2baf893769b695
Simon Fugl <sifug20@student.sdu.dk>
2021-12-03 13:37:54 +0100

    Got validation working with min

--- FrontEnd/app/Http/Controllers/ProductionController.php
+++ FrontEnd/app/Http/Controllers/ProductionController.php
@@ -102,6 +102,7 @@ class ProductionController extends Controller
     }
 
     public function beginProduction(Request $request){
+        //Validate form inputs
         $validated = $request->validate([
             'machineSpeed' => 'required|numeric|min:1|Integer',
             'amount' => 'required|numeric|min:1|Integer',
