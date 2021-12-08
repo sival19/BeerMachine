0a1ae501515b722890718bca1781b0b41c68f5fa
sival19 <sival19@student.sdu.dk>
2021-12-08 09:58:51 +0100

    UI WORKS 100 ish

--- BackEnd/src/main/java/opcUA/Runner.java
+++ BackEnd/src/main/java/opcUA/Runner.java
@@ -1,5 +1,7 @@
 package opcUA;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.pusher.client.channel.PusherEvent;
 import com.pusher.client.channel.SubscriptionEventListener;
 import pusher.IPusherManager;
@@ -16,43 +18,61 @@ public class Runner {
         //The data from pusher
         iPusherManager.SubscribePusher("App\\Events\\startProdEvent", pusherEvent -> {
             System.out.println(pusherEvent.getData());
+                JsonParser parser = new JsonParser();
+                JsonObject json = (JsonObject) parser.parse(pusherEvent.getData());
+                System.out.println(json);
+                float type = json.get("type").getAsFloat();
+                float speed = json.get("speed").getAsFloat();
+                float amount = json.get("amount").getAsFloat();
+                //start production on event
+                iopcuaManager.startSequence(1,type,amount,speed);
         }, "my-channel");
+
         iPusherManager.SubscribePusher("App\\Events\\stopButton", pusherEvent -> {
-            System.out.println(pusherEvent.getData());
+            System.out.println(pusherEvent.getData()
+
+            );
+            iopcuaManager.initiateCommand(3);
         }, "stopChannel");
+
         iPusherManager.SubscribePusher("App\\Events\\clearButton", pusherEvent -> {
             System.out.println(pusherEvent.getData());
+            iopcuaManager.initiateCommand(5);
         }, "clearChannel");
+
         iPusherManager.SubscribePusher("App\\Events\\abortButton", pusherEvent -> {
             System.out.println(pusherEvent.getData());
+            iopcuaManager.initiateCommand(4);
         }, "abortChannel");
+
         iPusherManager.SubscribePusher("App\\Events\\resetButton", pusherEvent -> {
             System.out.println(pusherEvent.getData());
+            iopcuaManager.initiateCommand(1);
         }, "resetChannel");
 
 
         //
-        iopcuaManager.initiateCommand(1);
+//        iopcuaManager.initiateCommand(1);
 //        iopcuaManager.startSequence(1,1,200,150);
 
         //read a node
 //        iopcuaManager.readNode("ns=6;s=::Program:product.produced");
 
         //subscribe to a node
-//        Thread t1 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:product.produced", "production.produced"));
-//        Thread t2 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Cube.Admin.ProdDefectiveCount", "production.Defective"));
-//        Thread t3 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:product.good", "production.Good"));
-//        Thread t4 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Data.Value.RelHumidity", "sensor.Humidity"));
-//        Thread t5 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Data.Value.Temperature", "sensor.Temperature"));
-//        Thread t6 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Data.Value.Vibration", "sensor.Vibration"));
+        Thread t1 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:product.produced", "production.produced"));
+        Thread t2 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Cube.Admin.ProdDefectiveCount", "production.Defective"));
+        Thread t3 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:product.good", "production.Good"));
+        Thread t4 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Data.Value.RelHumidity", "sensor.Humidity"));
+        Thread t5 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Data.Value.Temperature", "sensor.Temperature"));
+        Thread t6 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Data.Value.Vibration", "sensor.Vibration"));
         Thread t8 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Cube.Status.StateCurrent", "state.current"));
 
-//        t1.start();
-//        t2.start();
-//        t3.start();
-//        t4.start();
-//        t5.start();
-//        t6.start();
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+        t6.start();
         t8.start();
     }
 }
