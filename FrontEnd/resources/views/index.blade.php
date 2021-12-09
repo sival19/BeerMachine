4ac6ee519936d68f4007245e9d88321b9f4ff971
sival19 <sival19@student.sdu.dk>
2021-12-09 12:02:00 +0100

    viewcleanup

--- FrontEnd/resources/views/index.blade.php
+++ FrontEnd/resources/views/index.blade.php
@@ -1,6 +1,39 @@
 @extends('master')
 
 @section('content')
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
 
-<div>Hello World!</div>
+        <link rel="stylesheet" type="text/css" href="{{'/css/progressBars.css'}}">
+    </head>
+    <body>
+
+    <div class="progressBars">
+
+
+        <div class="progressBar">
+
+            <div class="barlyBar">100</div>
+        </div>
+        <div class="progressBar">
+            <div class="hopsBar">100</div>
+        </div>
+        <div class="progressBar">
+            <div class="maltBar">100</div>
+        </div>
+        <div class="progressBar">
+            <div class="wheatBar">100</div>
+        </div>
+        <div class="progressBar">
+            <div class="yeastBar">100</div>
+        </div>
+    </div>
+
+    </body>
+    </html>
 @endsection
