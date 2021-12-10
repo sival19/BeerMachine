32d7bf11b191857471a0448393afdecb05433645
ValonMorina <valon.morina301@hotmail.com>
2021-12-10 12:12:29 +0100

    test03

--- BackEnd/src/main/java/opcUA/Runner.java
+++ BackEnd/src/main/java/opcUA/Runner.java
@@ -29,8 +29,8 @@ public class Runner {
                 //start production on event
                 iopcuaManager.startSequence(1,type,amount,speed);
         }, "my-channel");
-//        iopcuaManager.initiateCommand(1);
-//        iopcuaManager.startSequence((float) 1,(float) 3,(float) 300,(float) 10);
+        iopcuaManager.initiateCommand(1);
+        iopcuaManager.startSequence((float) 1,(float) 3,(float) 300,(float) 160);
 
         iPusherManager.SubscribePusher("App\\Events\\stopButton", pusherEvent -> {
             System.out.println(pusherEvent.getData()
@@ -54,15 +54,15 @@ public class Runner {
             iopcuaManager.initiateCommand(1);
         }, "resetChannel");
 
-        iPusherManager.SubscribePusher("App\\Events\\readState", pusherEvent -> {
-            System.out.println(pusherEvent.getData());
-            try {
-                TimeUnit.SECONDS.sleep(2);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            iopcuaManager.readNode("ns=6;s=::Program:Cube.Status.StateCurrent", "state.read");
-        }, "readChannel");
+//        iPusherManager.SubscribePusher("App\\Events\\readState", pusherEvent -> {
+//            System.out.println(pusherEvent.getData());
+//            try {
+//                TimeUnit.SECONDS.sleep(2);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            iopcuaManager.readNode("ns=6;s=::Program:Cube.Status.StateCurrent", "state.read");
+//        }, "readChannel");
 
 
 
@@ -75,19 +75,19 @@ public class Runner {
 
         //subscribe to a node
         Thread t1 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:product.produced", "production.produced"));
-        Thread t2 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Cube.Admin.ProdDefectiveCount", "production.Defective"));
-        Thread t3 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:product.good", "production.Good"));
-        Thread t4 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Data.Value.RelHumidity", "sensor.Humidity"));
-        Thread t5 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Data.Value.Temperature", "sensor.Temperature"));
-        Thread t6 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Data.Value.Vibration", "sensor.Vibration"));
-        Thread t8 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Cube.Status.StateCurrent", "state.current"));
-
+//        Thread t2 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Cube.Admin.ProdDefectiveCount", "production.Defective"));
+//        Thread t3 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:product.good", "production.Good"));
+//        Thread t4 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Data.Value.RelHumidity", "sensor.Humidity"));
+//        Thread t5 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Data.Value.Temperature", "sensor.Temperature"));
+//        Thread t6 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Data.Value.Vibration", "sensor.Vibration"));
+//        Thread t8 = new Thread(() -> iopcuaManager.subscribe("ns=6;s=::Program:Cube.Status.StateCurrent", "state.current"));
+//
         t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-        t5.start();
-        t6.start();
-        t8.start();
+//        t2.start();
+//        t3.start();
+//        t4.start();
+//        t5.start();
+//        t6.start();
+//        t8.start();
     }
 }
